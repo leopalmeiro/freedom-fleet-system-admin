9b6bfa5de94197a739e0e99779ca90bdd2d70bb4
palmeiroleo.babel <leonardo.palmeiro@babel.es>
2020-03-21 20:21:18 +0100

    add some animations, and fix buttons layouts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,37 @@
-import { NewVehicleComponent } from './components/vehicle/new-vehicle/new-vehicle.component';
-import { VehicleComponent } from './components/vehicle/vehicle.component';
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AdminMainComponent } from './components/shared/admin-main/admin-main.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { NewVehicleComponent } from "./components/vehicle/new-vehicle/new-vehicle.component";
+import { VehicleComponent } from "./components/vehicle/vehicle.component";
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AdminMainComponent } from "./components/shared/admin-main/admin-main.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: AdminMainComponent,
-    children: [{
-      path: '',
-      component: DashboardComponent,
-    },
-    {
-      path: 'vehicles',
-      component: VehicleComponent,
-    },
-        {
-      path: 'vehicles/new',
-      component: NewVehicleComponent,
-    }
+    children: [
+      {
+        path: "",
+        component: DashboardComponent
+      },
+      {
+        path: "vehicles",
+        component: VehicleComponent
+      },
+      {
+        path: "vehicles/new",
+        component: NewVehicleComponent
+      },
+      {
+        path: "vehicles/edit/:id",
+        component: NewVehicleComponent
+      }
     ]
-
-}];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/vehicle/vehicle.component.ts
+++ src/app/components/vehicle/vehicle.component.ts
@@ -11,12 +11,19 @@ export interface Vehicle {
   plate: String;
 }
 
+
 const ELEMENT_DATA: Vehicle[] = [
   { id: 1, name: "Lambo", model: "aventor", year: 2020, plate: "tes-45200" },
   { id: 2, name: "Lambo", model: "aventor", year: 2018, plate: "tes-45200" },
   { id: 3, name: "Lambo", model: "aventor", year: 1255, plate: "tes-45200" },
   { id: 4, name: "Lambo", model: "aventor", year: 1920, plate: "tes-45200" },
-  { id: 5, name: "Lambo", model: "aventor01", year: 2020, plate: "tes-45200" }
+  { id: 5, name: "Lambo", model: "aventor01", year: 2020, plate: "tes-45200" },
+  { id: 6, name: "Lambo", model: "aventor", year: 2020, plate: "tes-45200" },
+  { id: 7, name: "Lambo", model: "aventor", year: 2018, plate: "tes-45200" },
+  { id: 8, name: "Lambo", model: "aventor", year: 1255, plate: "tes-45200" },
+  { id: 9, name: "Lambo", model: "aventor", year: 1920, plate: "tes-45200" },
+  { id: 10, name: "Lambo", model: "aventor01", year: 2020, plate: "tes-45200" }
+
 ];
 
 @Component({
@@ -25,9 +32,10 @@ const ELEMENT_DATA: Vehicle[] = [
   styleUrls: ["./vehicle.component.css"]
 })
 export class VehicleComponent implements OnInit {
-  displayedColumns: string[] = ["id", "name", "model", "year", "plate"];
+  displayedColumns: string[] = ['id', 'name', 'model', 'year', 'plate', 'actions' ];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
   @ViewChild(MatSort, { static: true }) sort: MatSort;
+
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
